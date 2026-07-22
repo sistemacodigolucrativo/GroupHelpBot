c65e37bdc7fe267bcc03d88bd4de62cbbffc5f84
sistemacodigolucrativo <sistemacodigolucrativo@gmail.com>
2026-07-22 13:15:37 -0300

    Adiciona 62 arquivos

---
+++ data/runflix/templates/textpro.js
@@ -0,0 +1,28 @@
+// Most take just `text`. A handful take `text1` + `text2` (marked below).
+module.exports = [
+  "xmasCard3d", "bloodFrosted", "halloweenFire", "darkGoldOnline", "jokerLogo", "wickerText",
+  "naturalLeaves", "fireworkSparkle", "skeletonText", "redFoilBalloon", "purpleFoilBalloon",
+  "pinkFoilBalloon", "greenFoilBalloon", "cyanFoilBalloon", "blueFoilBalloon", "goldFoilBalloon",
+  "steelText", "ultraGloss", "denimText", "decorateGreen", "decoratePurple", "peridotStone",
+  "rockText", "lavaText", "yellowGlass", "purpleGlass", "orangeGlass", "greenGlass", "cyanGlass",
+  "blueGlass", "redGlass", "purpleShinyGlass", "captainAmerica", "robotR2d2", "rainbowEqualizer",
+  "toxicText", "pinkSparkJewelry", "blueSparkJewelry", "greenSparkJewelry", "purpleSparkJewelry",
+  "goldSparkJewelry", "redSparkJewelry", "cyanSparkJewelry", "purpleGlassAlt", "decorativeGlass",
+  "chocolateCake", "strawberryText", "koiFish", "breadText", "horrorBlood", "neonLightText",
+  "thunderText", "boxText3d", "neonTextAlt", "roadWarning", "bokehText", "greenNeon",
+  "advancedGlow", "dropwater", "breakWall", "christmasGift", "honeyText", "plasticBag",
+  "horrorGift", "marbleSlabs", "marbleText", "iceCold", "fruitJuice", "rustyMetal", "abstraGold",
+  "biscuitText", "bagelText", "woodText", "sciFiText", "metalRainbow", "purpleGem", "shinyMetal",
+  "yellowJewelry", "silverJewelry", "redJewelry", "purpleJewelry", "orangeJewelry", "greenJewelry",
+  "cyanJewelry", "blueJewelry", "hotMetal", "hexaGolden", "blueGlitter", "purpleGlitter",
+  "pinkGlitter", "greenGlitter", "silverGlitter", "goldGlitter", "bronzeGlitter", "erodedMetal",
+  "carbonText", "pinkCandy", "blueMetal", "blueGem", "blackMetal", "glowingMetal3d", "chromeText3d",
+  // The following take text1 + text2 instead of a single `text`
+  "metalSilver3d", "metalRoseGold3d", "metalGold3d", "metalGalaxy3d", "lionMascot", "wolfLogoBw",
+  "wolfLogoGalaxy", "ninjaLogo", "steel3d",
+];
+
+module.exports.DUAL_TEXT = [
+  "metalSilver3d", "metalRoseGold3d", "metalGold3d", "metalGalaxy3d", "lionMascot", "wolfLogoBw",
+  "wolfLogoGalaxy", "ninjaLogo", "steel3d",
+];
